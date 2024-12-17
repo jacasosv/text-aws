ce5f402e3d9d42e47d891429238c2f5dc454bbff
juan <jacasos@hotmail.com>
2024-12-17 22:57:15 +0100

    Texto modificado

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',  (req, res) => {
-    res.send("Bienvenido y Hola Mundo");
+    res.send("<h1>Express con HTML !!!</h1>");
 });
 
 app.listen(3000);
